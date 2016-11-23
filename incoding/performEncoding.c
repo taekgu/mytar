87a082fe0853db8b73c66a22bc5be34a55757839
Jinwook Jeong <jjw543@naver.com>
2016-11-24 01:15:07 +0900

    performEncoding1

---
+++ incoding/performEncoding.c
@@ -0,0 +1,25 @@
+#define buf_SZ 100
+
+void performEncoding(char* fName)
+{
+	FILE* fin;
+	char buf[buf_SZ];
+
+	fin = fopen(fName, "rt");
+	if(fin == 0)
+	{
+		printf("Unable to open %s. Program terminated \n", fName);
+		return;
+	}
+	
+	while(fgets(buf, buf_SZ, fin) != 0)
+	{
+		int len = strlen(buf);
+		for( int i = 0;  i < len; i++)
+		{
+			
+		}
+	}
+
+	fclose(fin);
+}
