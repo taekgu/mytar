c1f0811d3ffd98e73e1bc0b4fe1c40f1b2b68170
Jeong Rok <king4719@naver.com>
2016-12-05 00:39:18 -0800

    noprint

--- head.h
+++ head.h
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #define buf_SZ 100
 #define num_ASCII 256
 
@@ -24,4 +29,7 @@ int codeBufIdx;
 
 int charFreq[num_ASCII];
 char *symCode[num_ASCII];
+
+void tree_loadHuff(struct node *cur, char c);
+void writeData(char *fName);
 #endif // _head_h
